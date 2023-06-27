0d45a9db62ce15a8ed7a578f68bc4ec4e0fa6c99
almaz <almazgimaev@gmail.com>
2023-06-27 18:20:39 +0300

    update options.py

--- src/options.py
+++ src/options.py
@@ -49,8 +49,8 @@ PREVIEWS_IS_DETECTION_TASK: bool = IS_DETECTION_TASK
 ### * Set up visualization params for ClassesPreview class ####
 CLASSES_PREVIEW_ROW_HEIGHT: int = None
 CLASSES_PREVIEW_PADDINGS: dict = None
-CLASSES_PREVIEW_ROWS: int = None
-CLASSES_PREVIEW_GAP: int = None
+CLASSES_PREVIEW_ROWS: int = 3
+CLASSES_PREVIEW_GAP: int = 10
 # default {"top": "10%", "bottom": "10%", "left": "10%", "right": "10%"}
 # set % or px as string values (e.i. "10%" or "10px")
 ###############################################################
