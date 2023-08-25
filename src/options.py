cc5434b9256a459c54add57db80f18927c10eeec
Arsenii Vladimirov <78355358+grokhi@users.noreply.github.com>
2023-08-25 11:52:15 +0600

    Update options.py

--- src/options.py
+++ src/options.py
@@ -5,7 +5,7 @@
 # * Preview class to visualize in SUMMARY.md overview section
 # * Literal["ClassesPreview", "HorizontalGrid", "SideAnnotationsGrid", "Poster"]
 # * If None, then preview_class will be set automatically to "ClassesPreview"
-PREVIEW_CLASS = "HorizontalGridAnimated"
+PREVIEW_CLASS = "ClassesPreview"
 
 IS_DETECTION_TASK: bool = None  # ? Set True if you want to visualize only bbox annotations
 
